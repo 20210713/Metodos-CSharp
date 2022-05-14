3e9b0bdcaf329728fd41134c294b038e3d050d9c
20210713 <103136976+20210713@users.noreply.github.com>
2022-05-13 20:53:04 -0400

    Add files via upload

---
+++ Tareas Metodos CSharp/Figuras geometricas metodos/Metodo Calculador/Metodo Calculador/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Metodo_Calculador
+{
+    class Program
+    {
+            public static void Suma(int a, int b)
+            {
+                int result = a + b;
+                Console.WriteLine($"Suma: {result}");
+            }
+            public static int Multiplicacion(int a, int b)
+            {
+                int result = a * b;
+                return result;
+            }
+            public static int Resta(int a, int b)
+            {
+                int result = a - b;
+                return result;
+            }
+            public static float Division(float a, float b)
+            {
+                float result = a / b;
+                return result;
+            }
+            static void Main(string[] args)
+            {
+                int num1, num2;
+                Console.WriteLine("escriba el primer numero");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("escriba el segundo numero");
+                num2 = int.Parse(Console.ReadLine());
+                Suma(num1, num2);
+                Console.WriteLine($"Resta: {Resta(num1, num2)}");
+                Console.WriteLine($"Multiplicacion: {Multiplicacion(num1, num2)}");
+                Console.WriteLine($"Division: {Division(num1, num2)}");
+            }
+        }
+    }
---
+++ Tareas Metodos CSharp/Figuras geometricas metodos/Metodos String/Metodos String/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Metodos_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal, profesion, nombre, tela, cosa, lugar, verbo, comida;
+
+            Console.WriteLine("Escribe un nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Escribe una profesion: ");
+            profesion = Console.ReadLine();
+
+            Console.WriteLine("Escribe una comida: ");
+            comida = Console.ReadLine();
+
+            Console.WriteLine("Escribe un tipo de tela: ");
+            tela = Console.ReadLine();
+
+            Console.WriteLine("Escribe el nombre de un lugar: ");
+            lugar = Console.ReadLine();
+
+            Console.WriteLine("Escribe el nombre de una cosa: ");
+            cosa = Console.ReadLine();
+
+            Console.WriteLine("Escribe un verbo: ");
+            verbo = Console.ReadLine();
+
+            Console.WriteLine("Escriba el nombre de un animal: ");
+            animal = Console.ReadLine();
+
+            Console.WriteLine(parte1(comida));
+            Console.WriteLine(parte2(nombre));
+            Console.WriteLine(parte3(lugar));
+            Console.WriteLine(parte4(animal));
+            Console.WriteLine(parte5(profesion));
+            Console.WriteLine(parte6(cosa));
+            Console.WriteLine(parte7(tela));
+            Console.WriteLine(parte8(verbo));
+        }
+
+        static string parte1 (string comida){
+            return "di " + comida + ", dijo el fotógrafo mientras la cámara destellaba! ";
+            }
+        static string parte2(string nombre)
+        {
+            return  nombre + " y había ido a";
+        }
+        static string parte3(string lugar)
+        {
+            return lugar + " para tomarnos fotos en mi cumpleaños.";
+        }
+        static string parte4(string animal)
+        {
+            return "La primera foto que realmente queríamos era una foto de nosotros vestidos como "+animal;
+        }
+        static string parte5(string profesion)
+        {
+            return " fingiendo ser una " + profesion+".";
+        }
+        static string parte6(string cosa)
+        {
+            return "Cuando vimos la segunda foto, era exactamente lo que quería. Ambos parecíamos " + cosa;
+        }
+        static string parte7(string tela)
+        {
+            return " vistiendo " + tela;
+        }
+        static string parte8(string verbo)
+        {
+            return " y " + verbo + " --exactamente lo que tenía en mente.";
+        }
+    }
+}
